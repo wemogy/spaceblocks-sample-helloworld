9e03d9ec8b29b02f3b41ddf2c126b027811161e0
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2024-04-02 14:29:34 +0200

    Initial commit

---
+++ SimplePermissions/Controllers/WeatherForecastController.cs
@@ -0,0 +1,107 @@
+using Microsoft.AspNetCore.Mvc;
+using SpaceBlocks.Libs.Sdk.Models;
+using SpaceBlocks.Permissions.Server;
+using SpaceBlocks.Permissions.Server.Model;
+
+namespace SimplePermissions.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly PermissionsClient _client;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+        var auth = new AuthenticationOptions(
+            "",
+            "",
+            "",
+            "permissions:management:read permissions:management:write");
+
+        _client = new PermissionsClient(
+            new Uri("https://cdwjnx.permissions.eu1.spaceblocks.cloud"),
+            "",
+            auth);
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public async Task<IEnumerable<WeatherForecast>> Get([FromQuery] string city, [FromQuery] string user)
+    {
+        await SeedAsync();
+
+        // Check, which permissions the user has
+        var permissions = await _client.PermissionApi.ListPermissionsAsync(
+            tenantId: "default",
+            resourceTypeId: "city", // "tenant" also doesn't work
+            resourceId: city,
+            subjectId: user);
+
+        var canGetCurrentForecast = permissions["city"].Contains("get-current-forecast");
+        var canGetFutureForecast = permissions["city"].Contains("get-future-forecast");
+
+        if (!canGetCurrentForecast && !canGetFutureForecast)
+        {
+            throw new UnauthorizedAccessException("You don't have permissions to access this resource.");
+        }
+
+        return Enumerable.Range(1, canGetFutureForecast ? 5 : 1).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+            City = city
+        })
+        .ToArray();
+    }
+
+    private async Task SeedAsync()
+    {
+        try
+        {
+            // Create Tenant
+            await _client.TenantApi.CreateTenantAsync(new CreateTenantRequest("default", "Default"));
+
+            // Create cities
+            await _client.ResourceApi.CreateResourceAsync("default", "city", new CreateResourceRequest("cansas", new CreateResourceParent("default")));
+            await _client.ResourceApi.CreateResourceAsync("default", "city", new CreateResourceRequest("seattle", new CreateResourceParent("default")));
+        }
+        catch (SpaceBlocks.Permissions.Server.Client.ApiException ex)
+        {
+            if (ex.ErrorCode == 409)
+            {
+                // Tenant already exists
+                return;
+            }
+
+            throw;
+        }
+
+        // Assign roles to cansas
+        await _client.ResourceApi.PatchResourceMembersAsync(
+            tenantId: "default",
+            resourceTypeId: "city",
+            id: "cansas",
+            new ResourceMembers(new Dictionary<string, List<string>>() {
+                    { "alice", [] },
+                    { "linda", ["future-forecast-viewer"] }
+            }));
+
+        // Assign roles to seattle
+        await _client.ResourceApi.PatchResourceMembersAsync(
+            tenantId: "default",
+            resourceTypeId: "city",
+            id: "seattle",
+            new ResourceMembers(new Dictionary<string, List<string>>() {
+                    { "alice", ["current-forecast-viewer"] },
+                    { "linda", ["future-forecast-viewer"] }
+            }));
+    }
+}
