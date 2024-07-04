9232bce8d69a4938b1198b5d48de5729819f0dd2
bennetrr <bennet@bennetr.me>
2024-07-04 17:47:46 +0200

    fix(python): Add missing scopes to authentication

--- python/main.py
+++ python/main.py
@@ -2,7 +2,7 @@ from datetime import date, timedelta
 from random import randint, choice
 
 from fastapi import FastAPI, HTTPException
-from spaceblocks_permissions_server import PermissionsClient, ClientAuthenticationOptions, ResourceMembers
+from spaceblocks_permissions_server import PermissionsClient, ClientAuthenticationOptions
 
 SUMMARIES = ['Freezing', 'Bracing', 'Chilly', 'Cool', 'Mild', 'Warm', 'Balmy', 'Hot', 'Sweltering', 'Scorching']
 
@@ -13,7 +13,8 @@ permissions_client = PermissionsClient(
     ClientAuthenticationOptions(
         api_key='<YOUR_API_KEY>',
         client_id='<YOUR_CLIENT_ID>',
-        client_secret='<YOUR_CLIENT_SECRET>'
+        client_secret='<YOUR_CLIENT_SECRET>',
+        scopes='permissions:management:read permissions:management:write'
     )
 )
 
@@ -31,10 +32,10 @@ def get_weather_forecast(user: str, city: str):
     can_get_future_forecast = 'future-forecast-viewer' in permissions['city']
 
     if not can_get_current_forecast and not can_get_future_forecast:
-        raise HTTPException(status_code=401, detail='No permission')
+        raise HTTPException(status_code=403, detail='No permission')
 
     return [{
                 'date': date.today() + timedelta(days=i+1),
                 'temperature': randint(-5, 38),
-                'summary': choice(SUMMARIES) 
+                'summary': choice(SUMMARIES)
             } for i in range(5 if can_get_future_forecast else 1)]
