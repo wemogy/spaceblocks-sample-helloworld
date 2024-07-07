3bc4be3226222a280b81a20db723201fc374ec32
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2024-07-07 19:29:26 +0200

    Update Node.js Sample to WeatherForecast scenario

--- node/index.js
+++ node/index.js
@@ -1,7 +1,5 @@
+const PermissionsClient = require('@spaceblocks/permissions-server').PermissionsClient;
 const express = require('express')
-const app = express()
-const port = 3000
-import { PermissionsClient, CreateTenantRequest, Members, MembersDictionary } from '@spaceblocks/permissions-server';
 
 const client = new PermissionsClient(
   '<YOUR_PERMISSIONS_URL>',
@@ -11,25 +9,47 @@ const client = new PermissionsClient(
     clientSecret: '<YOUR_CLIENT_SECRET>'
   });
 
-await client.tenantApi.patchTenantMembers(
-    "default",
-    new Members([
-      { "alice": ["internal"] },
-      { "linda": ["tax-accountant"] }
-    ]));
+const summaries = [
+  "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+];
+
+const app = express()
+const port = 3000
 
 app.get('/GetWeatherForecast/', async (req, res) => {
-
+  // Check, which permissions the user has for the city
   const permissions = await client.tenantApi.getTenantPermissions(
     "default",
     "city",
     req.query.city,
     req.query.user);
 
+  // Get permissions for the user
   const canGetCurrentForecast = permissions["city"].Contains("get-current-forecast");
+  const canGetFutureForecast = permissions["city"].Contains("get-future-forecast");
+  if (!canGetCurrentForecast && !canGetFutureForecast) {
+    res.status(403).send("You don't have permissions to access this resource.");
+    return;
+  }
 
+  // Depending on the permissions, return the forecast for 1 or 5 days
+  const forecastDays = canGetFutureForecast ? 5 : 1;
 
-  res.send('Hello World!')
+  // Generate the forecast  
+  const forecast = Array.from({ length: forecastDays }, (_, index) => {
+    const date = new Date();    
+    date.setDate(date.getDate() + index);
+    const temperatureC = Math.floor(Math.random() * (55 - (-20)) + (-20));
+    const summary = summaries[Math.floor(Math.random() * summaries.length)];
+    return {
+      Date: date,
+      TemperatureC: temperatureC,
+      TemperatureF: 32 + Math.round(temperatureC / 0.5556),
+      Summary: summary
+    };
+  });
+
+  res.send(forecast);
 })
 
 app.listen(port, () => {
