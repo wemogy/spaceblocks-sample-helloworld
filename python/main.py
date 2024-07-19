7758b46b3e8d57081f397137aa24c4b0d036ad4c
bennetrr <bennet@bennetr.me>
2024-07-19 14:04:03 +0200

    fix(python): Fix example

--- python/main.py
+++ python/main.py
@@ -1,8 +1,9 @@
 from datetime import date, timedelta
-from random import randint, choice
+from random import choice, randint
 
+import uvicorn
 from fastapi import FastAPI, HTTPException
-from spaceblocks_permissions_server import PermissionsClient, ClientAuthenticationOptions
+from spaceblocks_permissions_server import AuthenticationOptions, PermissionsClient
 
 SUMMARIES = ['Freezing', 'Bracing', 'Chilly', 'Cool', 'Mild', 'Warm', 'Balmy', 'Hot', 'Sweltering', 'Scorching']
 
@@ -10,11 +11,11 @@ app = FastAPI()
 
 permissions_client = PermissionsClient(
     '<YOUR_PERMISSIONS_URL>',
-    ClientAuthenticationOptions(
+    AuthenticationOptions(
         api_key='<YOUR_API_KEY>',
         client_id='<YOUR_CLIENT_ID>',
         client_secret='<YOUR_CLIENT_SECRET>',
-        scopes='permissions:management:read permissions:management:write'
+        scope='permissions:management:read permissions:management:write'
     )
 )
 
@@ -28,14 +29,18 @@ def get_weather_forecast(user: str, city: str):
         user
     )
 
-    can_get_current_forecast = 'current-forecast-viewer' in permissions['city']
-    can_get_future_forecast = 'future-forecast-viewer' in permissions['city']
+    can_get_current_forecast = 'get-current-forecast' in permissions['city']
+    can_get_future_forecast = 'get-future-forecast' in permissions['city']
 
     if not can_get_current_forecast and not can_get_future_forecast:
         raise HTTPException(status_code=403, detail='No permission')
 
     return [{
-                'date': date.today() + timedelta(days=i+1),
-                'temperature': randint(-5, 38),
-                'summary': choice(SUMMARIES)
-            } for i in range(5 if can_get_future_forecast else 1)]
+        'date': date.today() + timedelta(days=i + 1),
+        'temperature': randint(-5, 38),
+        'summary': choice(SUMMARIES)
+    } for i in range(5 if can_get_future_forecast else 1)]
+
+
+if __name__ == '__main__':
+    uvicorn.run(app)
